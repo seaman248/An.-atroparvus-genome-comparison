e20acb5a2d25330edf35d89353705ce2b2f30fdf
seaman248 <semen.bko@ya.ru>
2017-02-20 19:52:39 +0700

    remove prev changes

--- R/query_processing.R
+++ R/query_processing.R
@@ -1,16 +1,12 @@
-source('./R/functions/rowsWithBadChromosomes.R')
 source('./R/functions/arrange_chr.R')
+source('./R/functions/through.R')
 
 orths.fulltable <- read.csv('./data/processed/alb_atr_gam_sin(with coords).csv')
 
 # Arrange atroparvus
 atr_order <- read.csv2('./data/atr_order_with_x.csv')
 atr_chr <- arrange_chr(orths.fulltable[,c('X2.1')], atr_order[,c(1, 2)])
-
+orths.fulltable[,c(6:10)] <- through_num(orths.fulltable[,c(6:10)], atr_order)
 
 # Remove all rows with strange chromosomes in albimanus and gambiae
-goodChrs <- c('X', '2R', '2L', '3R', '3L')
-orths.goodChrs <- orths.fulltable[rowsWithBadChromosomes(orths.fulltable$X2, goodChrs),]
-orths.goodChrs <- orths.goodChrs[rowsWithBadChromosomes(orths.goodChrs[,'X2.2'], goodChrs),]
-
 
