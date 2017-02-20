46ced5619b9afce6615790b900f660042281cec0
seaman248 <semen.bko@ya.ru>
2017-02-20 18:53:40 +0700

    save full table of orths coords

--- R/query_coords.R
+++ R/query_coords.R
@@ -24,10 +24,20 @@ gene_coords <- lapply(sps, function(sp){
 
 names(gene_coords) <- sps
 
-### Find genes in orths table
+### Convert to list of data.frames
 
-lapply(sps, function(sp){
-  geneSet <- do.call('cbind', gene_coords[[sp]])[1:10,]
+gene_coords.frames <- lapply(sps, function(sp){
+  geneSet <- data.frame(do.call('cbind', gene_coords[[sp]]), stringsAsFactors = FALSE)
+  geneSet.new <- data.frame(matrix(NA, ncol=5, nrow = nrow(orths)))
   
-  
+  geneSet.new[match(geneSet[,1], orths[,sp]),] <- geneSet
+  geneSet.new
 })
+
+### Convert to one data frame to remove NA's
+
+gene_coords.data_frame <- na.omit(do.call('cbind', gene_coords.frames))
+
+### Save result data.frame
+
+write.csv(gene_coords.data_frame, './data/processed/alb_atr_gam_sin(with coords).csv', row.names = FALSE)
