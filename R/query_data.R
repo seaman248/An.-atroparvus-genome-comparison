b5ae341dc767e22ac99ffc1696b1014f209732ea
seaman248 <semen.bko@ya.ru>
2017-02-20 16:50:46 +0700

    get only set of orthologs

--- R/query_data.R
+++ R/query_data.R
@@ -11,15 +11,9 @@ sp_of_interest <- c(
 # get table of orthologs from biomart
 orths <- as.list(getOrthologs(sp_of_interest))
 
-# get all coordinates for genes and regroup that
-genes <- mapply(getGenesFromMart, geneIDs = orths, as.list(TRUE), dsName=names(orths))
-genes <- as.list(as.data.frame(genes))
-genes <-lapply(genes, as.data.frame)
+# make 
+orths <- data.frame(mapply(function(or){
+  or
+}, or=orths))
 
-# reorder genes accoring to ortologs order
-wgenes <- as.list(as.data.frame(mapply(genesAsOrth, genes=genes, orths=orths)))
-wgenes <- lapply(wgenes, as.data.frame)
-# scf -> chr in atroparvus and stephensi
-atr_order <- read.csv2('./data/atr_order_with_x.csv')
-
-wgenes$aatroparvus_eg_gene <- through_num(wgenes$aatroparvus_eg_gene, atr_order)
+write.csv2(orths, './data/processed/orthsList.csv')
