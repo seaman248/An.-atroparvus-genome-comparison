3b26986aade8249c7244583e46bf286700be5305
seaman248 <semen.bko@ya.ru>
2017-02-27 13:16:54 +0700

    create ignored dir

--- R/Clean/albimanus_clean.R
+++ R/Clean/albimanus_clean.R
@@ -22,6 +22,7 @@ alb_genes$chromosome_name <- revalue(alb_genes$chromosome_name, c(
 
 alb_genes[,c(3, 4)] <- seq_num(alb_genes[,c(2, 3, 4)], chrs = c(paste0('e', c(1:5))))
 
+dir.create('./R/Clean/output_data')
 write.csv2(alb_genes, './R/Clean/output_data/alb_clean.csv', row.names = F)
 
 rm(list=ls())
