60c43deacaf37263016fd46dd68932adfdd7563f
seaman248 <semen.bko@ya.ru>
2017-03-09 16:07:10 +0700

    work with matches

--- compare_plot/index.R
+++ compare_plot/index.R
@@ -3,6 +3,13 @@ source('./compare_plot/load_data.R')
 
 genes_to_compare <- tail(genes.trueorder[genes.trueorder[,7]==4, ], 5000)
 genes_to_compare <- genes_to_compare[genes_to_compare[, 2] != 4 & genes_to_compare[, 12] != 4, ]
-compare_plot(genes_to_compare, 2, sps=c('alb', 'atr', 'gam'))
 
+#compare_plot(genes_to_compare, 2, sps=c('alb', 'atr', 'gam'))
+
+tlist <- devide_tables(genes_to_compare)
+
+names(tlist) <- c('alb', 'atr', 'gam')
+
+
+orth_matches <- orth_matches(tlist, main_sp = 2)
 
