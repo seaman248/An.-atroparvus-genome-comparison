2bb92e247c16240f3ba3fd256bfa08dee763261f
seaman248 <semen.bko@ya.ru>
2017-03-10 13:13:24 +0700

    test how it works

--- compare_plot/index.R
+++ compare_plot/index.R
@@ -3,7 +3,7 @@ source('./compare_plot/load_data.R')
 
 genes_to_compare <- genes.trueorder[genes.trueorder[, 2]==4 & genes.trueorder[, 7]==4 & genes.trueorder[, 12]==4, ]
 genes_to_compare <- genes_to_compare[order(genes_to_compare[,8]*-1),]
-genes_to_compare <- genes_to_compare[1:500,]
+genes_to_compare <- genes_to_compare[1:100,]
 
 e4_orders <- list(
   alb = 1,
@@ -17,5 +17,5 @@ names(tlist) <- c('alb', 'atr', 'gam')
 
 matches <- orth_matches(tlist, main_sp = 2, order = e4_orders)
 
-compare_plot(tlist[c(2, 3)], matches[2], main_sp = 1)
-write.csv2(bind_cols(tlist[c(2, 3)]), './compare_plot/output_data/test_output/atr_gam.csv')
+compare_plot(tlist, matches, main_sp = 2)
+
