2c12b92dcac351d84a606d1bea6ec15e6e3ed1aa
seaman248 <semen.bko@ya.ru>
2017-04-06 11:16:42 +0700

    import blocks tables and try to import expression data

--- R/Secondary_stat/insecticide-malaria_genes/insecticides.R
+++ R/Secondary_stat/insecticide-malaria_genes/insecticides.R
@@ -1,63 +1,41 @@
 library(biomaRt)
 library(dplyr)
-source('R/Visualize/functions/parse_blocks.R')
 
 vb_host <- 'biomart.vectorbase.org' 
-mart <- useMart(listMarts(host=vb_host)$biomart[1], host = vb_host, dataset = 'agambiae_eg_gene') 
-
-genes <- read.table('./R/Clean/output_data/GRIMM.txt', stringsAsFactors = F)
-blocks <- parse_blocks(read.table('./R/GRIMM/output_data/blocks/blocks.txt'))
-orthologs <- read.csv2('./R/Query/output_data/orthologs.csv', stringsAsFactors = F)
-
-
-pfam_ids <- getBM(
-  attributes = c('ensembl_gene_id', 'pfam_pf'),
-  mart = mart
-)
-
-gam_regions <- getBM(
-  attributes = c('ensembl_gene_id', 'band'),
-  mart = mart
-)
-
-MIN_BIG_BLOCK <- 500000
-
-atr_blocks <- blocks[[2]]
-atr_big_blocks <- atr_blocks[atr_blocks$end > MIN_BIG_BLOCK, ]
-
-
-gam_ids <- apply(atr_big_blocks, 1, function(row){
-  end <- as.numeric(row[2]) + as.numeric(row[3])
-  alb_ids <- genes[genes$V7 == anchor_coords$V7, 1]
-
-  res <- data.frame(
-    ids = orthologs[which(!is.na(match(orthologs[, 1], alb_ids))), 3]
-  )
-  res$band <- gam_regions[match(res$ids, gam_regions[, 1]), 2]
-  res$pfam <- pfam_ids[match(res$ids, pfam_ids[, 1]), 2]
-  res
-})
-
-lapply(gam_ids, function(df){
-  df <- df %>% slice(which(!is.na(match(df$pfam, IRP)))) %>%
-    select(pfam, band) %>%
-    group_by(pfam, band)
-})
-
-# insecticide-resistent protein ids:
+genes_mart <- useMart(listMarts(host=vb_host)$biomart[1], host = vb_host, dataset = 'aatroparvus_eg_gene')
 
 IRP <- c('PF00756', 'PF13417', 'PF00067', 'PF06512')
-
-expected_freq <- length(which(!is.na(match(pfam_ids[, 2], IRP)))) / length(pfam_ids[, 2])
-IRP_blocks <- unlist(lapply(gam_ids, function(df){
-  actual_freq <- length(which(!is.na(match(df$pfam, IRP)))) / nrow(df)
-  actual_freq / expected_freq
-}))
+UCB <- read.table('./R/Secondary_stat/max_blocks.txt', header = T)
+atr_genes <- getBM(attributes = c('ensembl_gene_id', 'chromosome_name', 'start_position', 'end_position'), mart = genes_mart)
 
 
-lapply(gam_ids[IRP_blocks > 2], function(IRP_ids){
-  # table(gam_regions[match(IRP_ids[, 2], gam_regions[, 1]), 2])
-  IRP_ids
+atr_blocks <- apply(UCB, 1, function(ucb_row){
+  
+  coord_string <- paste0(c(ucb_row[-1], 1), collapse = ':')
+  print(ucb_row[1])
+  
+  data.frame(
+    block = ucb_row[1],
+    getBM(
+    attributes = c('ensembl_gene_id', 'chromosome_name', 'start_position', 'end_position', 'agambiae_eg_gene'),
+    filters = c('chromosomal_region'),
+    values = coord_string,
+    mart = genes_mart
+  ))
+  
 })
+
+atr_blocks <- bind_rows(atr_blocks)
+
+exp_mart <- useMart('expression', host=vb_host, dataset = 'vb')
+dev_exp <- getBM(
+  attributes = c('p-value', 'ensembl_gene_id'),
+  filters = c('hyb_annotation_type', 'experiment_id'),
+  values = list(
+    c('DevelopmentalStage'),
+    c(29)
+  ),
+  mart = exp_mart
+)
 
 
