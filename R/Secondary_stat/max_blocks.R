baa6ad091903669caceee010fe1640ab77e6e490
seaman248 <semen.bko@ya.ru>
2017-04-23 15:35:28 +0700

    Revert "stage local"
    This reverts commit 5e09fbcc884c6606ea90a7dbdf507a2cbffd3cc3.

--- R/Secondary_stat/max_blocks.R
+++ R/Secondary_stat/max_blocks.R
@@ -19,13 +19,11 @@ atr_grimm <- transform(atr_grimm, V7 = as.numeric(V7), V8 = as.numeric(V8), V9 =
 # Находим какие ортологи генов альбимануса задействованы в крупных блоках
 alb_ids_set <- apply(topmost_blocks, 1, function(row){
   
-  start = as.numeric(row[3])
-  end = start + as.numeric(row[4])
+  start = as.numeric(row[2])
+  end = start + as.numeric(row[3])
   
   alb_ids <- filter(atr_grimm, V7 >= start, V7 <= end)[, 1]
 })
-
-names(alb_ids_set) <- topmost_blocks[, 1]
 
 # Находим по таблице ортологов соответствующие гены у An. atroparvus
 atr_genes_blocks <- lapply(alb_ids_set, function(alb_ids){
@@ -42,18 +40,15 @@ names(atr_genes_blocks) <- rownames(atr_blocks[match(topmost_blocks$V8, atr_bloc
 block_coords <- bind_rows(lapply(names(atr_genes_blocks), function(n){
   block_genes <- atr_genes_blocks[[n]]
   scfs <- table(as.character(block_genes[, 2]))
-  # scfs <- scfs[scfs>3]
+  scfs <- scfs[scfs>3]
   bind_rows(lapply(names(scfs), function(scf){
     data.frame(
       block = n,
       scf = scf,
       start = min(block_genes[block_genes[, 2] == scf, 3]),
-      end = max(block_genes[block_genes[, 2] == scf, 4]),
-      ngenes = scfs[scf]
+      end = max(block_genes[block_genes[, 2] == scf, 4])
     )
   }))
 }))
-
-block_coords[block_coords$ngenes > 3, ]
 
 write.table(block_coords, file='./R/Secondary_stat/max_blocks.txt', row.names = F, quote = F)
--- R/Secondary_stat/max_blocks_statistics.R
+++ R/Secondary_stat/max_blocks_statistics.R
@@ -1,8 +1,6 @@
 library(ggplot2)
 library(dplyr)
 atr_blocks <- read.table('./R/GRIMM/output_data/blocks/blocks.txt')[,c(6:9)]
-atr_grimm <- read.table('./R/Clean/output_data/GRIMM.txt', stringsAsFactors = F)[, c(1, 6:9)]
-orthologs <- read.csv2('./R/Query/output_data/orthologs.csv', stringsAsFactors = F)
 
 o <- function(size){
   size[size >= quantile(size, probs = c(0.98))[1]]
@@ -18,4 +16,6 @@ ggplot(atr_blocks, aes(x = V6, y = V8))+
 topmost_blocks <- atr_blocks %>%
   group_by(V6) %>%
   filter(V8 >= o(V8))
+
+
 
