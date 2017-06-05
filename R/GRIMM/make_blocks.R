c476535f1382491d8d043d8da4314693825bb178
seaman248 <semen.bko@ya.ru>
2017-06-05 16:49:56 +0700

    make grimm without anchors

--- R/GRIMM/make_blocks.R
+++ R/GRIMM/make_blocks.R
@@ -2,9 +2,10 @@ source('./R/GRIMM/functions/make_grimm_command.R')
 
 
 # Make anchors
-system(make_grimm_command(), wait = T)
+# system(make_grimm_command(), wait = T)
 
 # Make blocks
-system(make_grimm_command(A=F, input='/R/GRIMM/output_data/anchors/unique_coords.txt'), wait=T)
+# system(make_grimm_command(A=F, input='/R/GRIMM/output_data/anchors/unique_coords.txt'), wait=T)
+system(make_grimm_command(A=F, input='/R/Clean/output_data/GRIMM.txt'), wait=T)
 
-rm(list=ls())
+# rm(list=ls())
