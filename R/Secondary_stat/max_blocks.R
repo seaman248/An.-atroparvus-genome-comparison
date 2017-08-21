4ffbd91ae0a597c142bc5e8ad9ee37f17764829e
seaman248 <semen.bko@ya.ru>
2017-08-21 15:54:29 +0700

    7

--- R/Secondary_stat/max_blocks.R
+++ R/Secondary_stat/max_blocks.R
@@ -2,12 +2,21 @@ library(dplyr)
 
 
 ###
-atr_grimm <- read.table('./R/Clean/output_data/GRIMM.txt', stringsAsFactors = F, header = T)[, c(1, 6:9)]
+
+
+atr_blocks <- read.table('./R/GRIMM/output_data/blocks/blocks.txt')[,c(1, 6:9)]
+atr_grimm <- read.table('./R/Clean/output_data/GRIMM.txt', stringsAsFactors = F)[, c(1, 6:9)]
 orthologs <- read.csv2('./R/Query/output_data/orthologs.csv', stringsAsFactors = F)
 
 alb_genes <- read.csv('./R/Query/output_data/albimanus_genes.csv')
 atr_genes <- read.csv('./R/Query/output_data/atroparvus_genes.csv')
 gam_genes <- read.csv('./R/Query/output_data/gambiae_genes.csv')
+
+topmost_blocks <- atr_blocks[atr_blocks[,3] > 1000000, ]
+
+topmost_blocks <- data.frame(atr_blocks %>%
+  group_by(V6) %>%
+  filter(V8 >= mean(V8) + 3 * sd(V8)))
 
 atr_grimm <- transform(atr_grimm, V7 = as.numeric(V7), V8 = as.numeric(V8), V9 = as.numeric(V9))
 
@@ -19,11 +28,13 @@ atr_grimm <- transform(atr_grimm, V7 = as.numeric(V7), V8 = as.numeric(V8), V9 =
 # Находим какие ортологи генов альбимануса задействованы в крупных блоках
 alb_ids_set <- apply(topmost_blocks, 1, function(row){
   
-  start = as.numeric(row[2])
-  end = start + as.numeric(row[3])
+  start = as.numeric(row[3])
+  end = start + as.numeric(row[4])
   
   alb_ids <- filter(atr_grimm, V7 >= start, V7 <= end)[, 1]
 })
+
+names(alb_ids_set) <- topmost_blocks[, 1]
 
 # Находим по таблице ортологов соответствующие гены у An. atroparvus
 atr_genes_blocks <- lapply(alb_ids_set, function(alb_ids){
@@ -40,15 +51,18 @@ names(atr_genes_blocks) <- rownames(atr_blocks[match(topmost_blocks$V8, atr_bloc
 block_coords <- bind_rows(lapply(names(atr_genes_blocks), function(n){
   block_genes <- atr_genes_blocks[[n]]
   scfs <- table(as.character(block_genes[, 2]))
-  scfs <- scfs[scfs>3]
+  # scfs <- scfs[scfs>3]
   bind_rows(lapply(names(scfs), function(scf){
     data.frame(
       block = n,
       scf = scf,
       start = min(block_genes[block_genes[, 2] == scf, 3]),
-      end = max(block_genes[block_genes[, 2] == scf, 4])
+      end = max(block_genes[block_genes[, 2] == scf, 4]),
+      ngenes = scfs[scf]
     )
   }))
 }))
+
+block_coords[block_coords$ngenes > 3, ]
 
 write.table(block_coords, file='./R/Secondary_stat/max_blocks.txt', row.names = F, quote = F)
--- R/Secondary_stat/max_blocks_statistics.R
+++ R/Secondary_stat/max_blocks_statistics.R
@@ -1,12 +1,15 @@
 library(ggplot2)
 library(dplyr)
-g_table <- read.table('./R/Clean/output_data/GRIMM.txt', header = T)
-g_blocks <- read.table('./R/GRIMM/output_data/blocks/blocks.txt')
-q_orthologs <- read.csv2('./R/Query/output_data/orthologs.csv', stringsAsFactors = F)
+atr_blocks <- read.table('./R/GRIMM/output_data/blocks/blocks.txt')[,c(6:9)]
+atr_grimm <- read.table('./R/Clean/output_data/GRIMM.txt', stringsAsFactors = F)[, c(1, 6:9)]
+orthologs <- read.csv2('./R/Query/output_data/orthologs.csv', stringsAsFactors = F)
 
-o <- function(size){
-  size[size >= quantile(size, probs = c(0.98))[1]]
-}
+alb_genes <- read.csv('./R/Query/output_data/albimanus_genes.csv')
+atr_genes <- read.csv('./R/Query/output_data/atroparvus_genes.csv')
+gam_genes <- read.csv('./R/Query/output_data/gambiae_genes.csv')
+atr_grimm <- transform(atr_grimm, V7 = as.numeric(V7), V8 = as.numeric(V8), V9 = as.numeric(V9))
+
+#mean(atr_blocks$V8) + sd(atr_blocks$V8) * 3
 
 ggplot(g_blocks, aes(x = V6, y = V8))+
   geom_boxplot(outlier.size  = 0, outlier.alpha = 0)+
@@ -34,55 +37,11 @@ alb_ids <- apply(atr_topmost_blocks, 1, function(row){
 
 names(alb_ids) <- atr_topmost_blocks$V1
 
-##### Вычисляем ids An. gambiae
+UCBs <- atr_blocks %>%
+  group_by(V6) %>%
+  filter(V8 >= mean(V8) + 3 * sd(V8))
 
 gam_ids <- lapply(alb_ids, function(ids){
   ids <- ids[, 1]
   q_orthologs[which(!is.na(match(q_orthologs$aalbimanus_eg_gene, ids))), 3]
 })
-
-gam_ids <- bind_rows(lapply(names(gam_ids), function(n){
-  data.frame(
-    block = n,
-    ids = gam_ids[[n]]
-  )
-}))
-
-block_stat <- gam_ids %>%
-  group_by(block) %>%
-  summarize(n())
-
-# agam_expression <- read.table('./R/Secondary_stat/Anopheles-gambiae_MSM-STATS_VB-2017-04.txt', sep = '\t', header = T)
-
-exp_blocks <- bind_cols(apply(agam_expression[, 2:5], 2, function(col){
-  exp_data <- data.frame(agam_expression[, 1], col) %>%
-    filter(!is.na(col), col<= 0.05)
-  exp_ids <- as.character(exp_data[, 1])
-  
-  matches <- which(!is.na(match(
-    exp_ids, gam_ids$ids
-  )))
-  
-  exp_stat <- gam_ids %>%
-    slice(matches) %>%
-    group_by(block) %>%
-    summarize(n())
-  
-  matches <- match(
-    block_stat$block, exp_stat$block
-  )
-  
-  stat <- data.frame(block_stat, exp_stat[matches, 2])
-  
-  colnames(stat) <- c('block', 'exp', 'obs')
-  
-  n_exp <- stat %>%
-    mutate(n_exp = obs / exp * 100) %>%
-    select(n_exp)
-  
-  if(any(na.omit(n_exp) > 90)){
-    which(!is.na(n_exp[n_exp > 90]))
-  }
-}))
-
-
