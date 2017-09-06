bb048c45236ab83168edbeaa903c1a776896708a
seaman248 <semen.bko@ya.ru>
2017-09-06 14:17:57 +0700

    remove duplicates

--- R/Visualize/visualize_blocks.R
+++ R/Visualize/visualize_blocks.R
@@ -4,7 +4,46 @@ source('./R/Visualize/functions/parse_blocks.R')
 
 MIN_BLOCK_SIZE = 1000000
 
-blocks <- parse_blocks(read.table('./R/GRIMM/output_data/blocks/blocks.txt'))
+
+# rows_to_bind <- list(
+#   c(334, 335, 336, 337), # e2 e4  e4
+#   c(768, 773, 774), # e5 e3  e3
+#   c(769, 770, 771, 772, 775) # e5 e4  e3
+# )
+
+blocks_file <- read.table('./R/GRIMM/output_data/blocks/blocks.txt', row.names = 1)
+
+V3_to_remove <- unlist(c(
+V3_alb_overlaps <- blocks_file %>%
+  mutate(alb_end = V3 + V4) %>%
+  arrange(V3) %>%
+  mutate(er_alb = c(alb_end[1:length(alb_end)-1] - V3[2:length(V3)], 0)) %>%
+  filter(er_alb > 0) %>%
+  dplyr::select(V3)
+,
+V3_atr_overlaps <- blocks_file %>%
+  mutate(atr_end = V7 + V8)%>%
+  arrange(V7) %>%
+  mutate(er_atr = c(atr_end[1:length(atr_end)-1] - V7[2:length(V7)], 0)) %>%
+  filter(er_atr > 0) %>%
+  dplyr::select(V3)
+,
+V3_gam_overlaps <- blocks_file %>%
+  mutate(gam_end = V11 + V12) %>%
+  arrange(V11) %>%
+  mutate(er_gam = c(gam_end[1:length(gam_end)-1] - V11[2:length(V11)], 0)) %>%
+  filter(er_gam > 0) %>%
+  dplyr::select(V3)
+))
+
+rows_to_remove <- c(match(V3_to_remove, blocks_file$V3), 410, 335, 336, 334, 773, 774, 770, 771, 772, 775)
+
+# blocks_file2 <- read.table('./data/blocks.txt', row.names = 1)
+# rows_to_remove <- as.numeric(row.names(blocks_file[(blocks_file[, 1] != blocks_file[, 5]) | (blocks_file[, 5] != blocks_file[, 9]), ]))
+# blocks_file2[(blocks_file2[, 1] != blocks_file2[, 5]) | (blocks_file2[, 5] != blocks_file2[, 9]), ]
+
+
+blocks <- parse_blocks(read.table('./R/GRIMM/output_data/blocks/blocks.txt')[-rows_to_remove, ])
 
 names(blocks) <- c('alb', 'atr', 'gam')
 
